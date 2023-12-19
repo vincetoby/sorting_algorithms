10a04cecc65a8f3bf7f34d82095a022a40129608
M0111Ange <angemukundente@gmail.com>
2023-12-19 04:21:53 -0800

    102-counting_sort.c

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,11 +1,5 @@
 #include "sort.h"
-
-/**
- * get_max - Get the maximum value in an array of integers.
- * @array: An array of integers.
- * @size: The size of the array.
- * Return: The maximum integer in the array.
- */
+#include <stdlib.h>
 
 int get_max(int *array, int size)
 {
@@ -19,14 +13,6 @@ int get_max(int *array, int size)
 
 	return (max);
 }
-
-/**
- * counting_sort - Sort an array of integers in ascending order
- * using the counting sort algorithm.
- * @array: An array of integers.
- * @size: The size of the array.
- * Description: Prints the counting array after setting it up.
- */
 
 void counting_sort(int *array, size_t size)
 {
